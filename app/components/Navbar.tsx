0848ccc4e8e69cfd7ed5bb1a767f4ee2ebd624c1
1VishalSD <ilearnvk@gmail.com>
2025-01-31 17:11:20 +0530

    INTIALIZE THE FILE FOR THE NAVBAR

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,31 @@
 import Link from "next/link";
+import { deflate } from "zlib";
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <Link href={"/service"}>Services</Link>
-      <Link href={"/work"}>Work</Link>
-      <Link href={"/resume"}>Resume</Link>
-      <Link href={"/skill"}>Skill</Link>
-      <Link href={"/testimonils"}>Testimonils</Link>
-      <Link href={"/contact"}>Contact</Link>
+    <nav className="navbar">
+      <Link href="/">Logo</Link>
+      <ul>
+        <li>
+          <Link href="/services">Service</Link>
+        </li>
+        <li>
+          <Link href="/work">Work</Link>
+        </li>
+        <li>
+          <Link href="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link href="/skill">Skill</Link>
+        </li>
+        <li>
+          <Link href="/testimonials">Testimonials</Link>
+        </li>
+        <li>
+          <Link href="/contacts">Contacts</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
+
+export default Navbar;
